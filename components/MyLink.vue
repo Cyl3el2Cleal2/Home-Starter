fc1dd65cb11e0dcd578f6112d5428ff88a5513d5
cyl3el2cleal2 <nathachian@gmail.com>
2020-07-22 22:00:22 +0700

    Init home page

---
+++ components/MyLink.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    :class="itemClass"
+    @click="openLink(link)"
+    @contextmenu="openNewTab($event, link)"
+  >
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      itemClass:
+        'text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 menu-close',
+    }
+  },
+  methods: {
+    async openNewTab(e, linkUrl) {
+      await e.preventDefault()
+      return await window.open(linkUrl)
+    },
+    async openLink(linkUrl) {
+      return await window.open(linkUrl, '_self')
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <div :class="boxColClass">
+      <div v-for="(linkRow, i) in linkData" :key="i" :class="boxRowClass">
+        <MyLink
+          v-for="linkItem in linkRow"
+          :key="linkItem.title"
+          v-bind="linkItem"
+        >
+        </MyLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      boxColClass: 'flex flex-col bg-gray-200',
+      boxRowClass: 'flex justify-center bg-gray-200',
+      linkData: [
+        [
+          {
+            title: 'youtube',
+            link: 'https:www.youtube.com',
+          },
+          {
+            title: 'youtube',
+            link: 'https:www.youtube.com',
+          },
+        ],
+        [
+          {
+            title: 'youtube',
+            link: 'https:www.youtube.com',
+          },
+        ],
+        [
+          {
+            title: 'youtube',
+            link: 'https:www.youtube.com',
+          },
+        ],
+      ],
+    }
+  },
+  methods: {
+    async openNewTab(e, linkUrl) {
+      await e.preventDefault()
+      return await window.open(linkUrl)
+    },
+    async openLink(linkUrl) {
+      return await window.open(linkUrl, '_self')
+    },
+  },
+}
+</script>
+
+<style>
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+
+.menu-close {
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+</style>
